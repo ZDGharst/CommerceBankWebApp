ed0ae03c2ef6158ffbc432655a852804f269bfde
zach.gharst <zdgharst@gmail.com>
2021-02-18 13:07:55 -0600

    Remove https redirects
    * They are handled by Apache automatically.

--- src/Commerce/Startup.cs
+++ src/Commerce/Startup.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -39,7 +38,6 @@ namespace Commerce
                 app.UseExceptionHandler("/Home/Error");
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
