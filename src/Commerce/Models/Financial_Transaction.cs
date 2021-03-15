f5855b0877499046e0b484f5a19d06d212e860ad
zach.gharst <zdgharst@gmail.com>
2021-03-14 22:25:56 -0500

    Fix precision warning with money types

--- src/Commerce/Models/Financial_Transaction.cs
+++ src/Commerce/Models/Financial_Transaction.cs
@@ -1,5 +1,6 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 
 using Commerce.Data;
@@ -12,8 +13,12 @@ namespace Commerce.Models
     public int Account_Id { get; set; }
     public DateTime TimeStamp { get; set; }
     public string Type { get; set; }
-    public decimal Amount { get; set; }
-    public decimal Balance_After { get; set; }
     public string Description { get; set; }
+
+    [Column(TypeName = "decimal(18,2)")]
+    public decimal Amount { get; set; }
+
+    [Column(TypeName = "decimal(18,2)")]
+    public decimal Balance_After { get; set; }
   }
 }
