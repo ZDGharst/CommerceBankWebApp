a7e66b1451ab7bccf28af01327608fd91d50aca0
zach.gharst <zdgharst@gmail.com>
2021-03-21 17:09:14 -0500

    Initialize DBA tools

---
+++ src/DatabaseAdministration/Data/DbaContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using DatabaseAdministration.Models;
+
+namespace DatabaseAdministration.Data
+{
+    public class DbaContext : DbContext
+    {
+        public DbaContext (DbContextOptions<DbaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Account> Account { get; set; }
+        public DbSet<Financial_Transaction> Financial_Transaction { get; set; }
+    }
+}
---
+++ src/DatabaseAdministration/Models/Account.cs
@@ -0,0 +1,21 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseAdministration.Models
+{
+  public class Account
+  {
+    public int Id { get; set; }
+    public int Customer_Id { get; set; }
+    public string Account_Type { get; set; }
+    public string Nickname { get; set; }
+
+    [Column(TypeName = "decimal(18,2)")]
+    public decimal Interest_rate { get; set; }
+
+    [Column(TypeName = "decimal(18,2)")]
+    public decimal Balance { get; set; }
+  }
+}
