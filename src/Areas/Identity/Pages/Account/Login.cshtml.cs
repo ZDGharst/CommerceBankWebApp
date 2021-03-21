4d77f277d8919a06ef3f2ad107430eff637d1ed4
Banana Klein <benaiahkilen@gmail.com>
2021-03-20 22:35:01 -0500

    Updated Login page layout

--- src/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ src/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -44,10 +44,12 @@ namespace Commerce_WebApp.Areas.Identity.Pages.Account
         {
             [Required]
             [EmailAddress]
+            [Display(Name = "Email Address:")]
             public string Email { get; set; }
 
             [Required]
             [DataType(DataType.Password)]
+            [Display(Name = "Password:")]
             public string Password { get; set; }
 
             [Display(Name = "Remember me?")]
