18619ed262f6729a7086d7b885e6507aef367707
zach.gharst <zdgharst@gmail.com>
2021-04-23 12:14:31 -0500

    Redirect to log in page if user isn't logged in

--- src/CommerceWebApp/Controllers/AccountController.cs
+++ src/CommerceWebApp/Controllers/AccountController.cs
@@ -26,7 +26,7 @@ namespace Commerce_WebApp.Controllers
             var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
             if (userId == null)
             {
-                return View("RequireLogin");
+                return RedirectToAction("Login", "Login");
             }
 
             var Customer_Account = await _context.Customer_Account.FirstOrDefaultAsync(m => m.Customer_Id == userId && m.Account_Id == id);
