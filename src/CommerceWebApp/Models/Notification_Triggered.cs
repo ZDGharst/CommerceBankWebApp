b2cb80e579ddc68b9f2de19e52bd29b95ecf4b78
zach.gharst <zdgharst@gmail.com>
2021-05-02 12:37:38 -0500

    Add model for notications triggered

--- src/CommerceWebApp/Models/Notification_Triggered.cs
+++ src/CommerceWebApp/Models/Notification_Triggered.cs
@@ -5,18 +5,15 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Commerce_WebApp.Models
 {
-  public class Notification_Rule
+  public class Notification_Triggered
   {
     public int Id { get; set; }
-    public string Customer_Id { get; set; }
     public string Type { get; set; }
     public string Condition { get; set; }
-    public string Message { get; set; }
 
     [Column(TypeName = "decimal(18,2)")]
     public decimal Value { get; set; }
-    public bool Notify_Text { get; set; }
-    public bool Notify_Email { get; set; }
-    public bool Notify_Web { get; set; }
+
+    public int Count { get; set; }
   }
 }
